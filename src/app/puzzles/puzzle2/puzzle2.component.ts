2ce2198e2eba10b5a895d8c7264aa346a28422f7
Stone-Monkey1 <92111289+Stone-Monkey1@users.noreply.github.com>
2022-04-17 20:05:18 -0400

    Make climber's foot clickable

--- src/app/puzzles/puzzle2/puzzle2.component.ts
+++ src/app/puzzles/puzzle2/puzzle2.component.ts
@@ -10,6 +10,28 @@ export class Puzzle2Component implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    this.SpawnRandomButton();
   }
 
+  SpawnRandomButton() {
+    var b = document.querySelector("button");
+    b.onload
+    function change() {
+      var i = Math.floor(Math.random() * window.innerWidth);
+      var j = Math.floor(Math.random() * window.innerHeight);
+      b.style.left = i + "px";
+      b.style.top = j + "px";
+    }
+  }
+
+  RandomButtonLocation() {
+    var b = document.querySelector("button");
+    b.addEventListener("click", change);
+    function change() {
+      var i = Math.floor(Math.random() * window.innerWidth);
+      var j = Math.floor(Math.random() * window.innerHeight);
+      b.style.left = i + "px";
+      b.style.top = j + "px";
+    }
+  }
 }
