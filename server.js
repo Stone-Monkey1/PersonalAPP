8aa5f211d3ab6777f8de3dd56c69132d60c0dc4a
Stone-Monkey1 <92111289+Stone-Monkey1@users.noreply.github.com>
2022-05-10 11:32:52 -0400

    Give up publishing to gitpages

--- server.js
+++ server.js
@@ -2,11 +2,11 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static(__dirname + 'docs'));
+app.use(express.static(__dirname + '/dist/client'));
 
 app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname +
-    'docs/index.html'));
+    '/dist/client/index.html'));
 });
 
 app.listen(process.env.PORT || 8080);
