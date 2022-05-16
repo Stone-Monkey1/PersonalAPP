116c463177b931e613428142ea221461ad579ae3
Stone-Monkey1 <92111289+Stone-Monkey1@users.noreply.github.com>
2022-05-16 14:40:26 -0400

    Animated Letters

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,14 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, AfterViewInit {
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  ngAfterViewInit() {
+    this.changeClass();
+  }
+
+  changeClass() {
+
+    window.onload = function () {
+      const elements = document.getElementsByClassName('wordPop');
+
+      if (elements) {
+        for (let i = 0; i <= elements.length; i++) {
+          elements[i].addEventListener('mouseover', () => {
+            elements[i].classList.add('animated');
+          });
+
+          elements[i].addEventListener('animationend', () => {
+            elements[i].classList.remove('animated');
+          });
+        }
+      }
+    }
   }
 }
